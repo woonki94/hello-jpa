965648a8f89b704e3e76c9d76e4522ee486ba1b6
woonki94 <woonki94@naver.com>
2022-01-21 17:51:54 +0900

    initial settings

--- src/main/java/com/hellojpa/JpaMain.java
+++ src/main/java/com/hellojpa/JpaMain.java
@@ -2,7 +2,9 @@ package com.hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -10,13 +12,34 @@ public class JpaMain {
 
        EntityManager em = emf.createEntityManager();
 
-       Member member = new Member();
-       member.setId(1L);
-       member.setName("HelloA");
-       em.persist(member);
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //저장
+            //Member member = new Member();
+            //member.setId(2L);
+            //member.setName("HelloB");
+            //em.persist(member);
+            /**
+             * 수정
+             */
+            //Member findMember = em.find(Member.class, 1L);
+            //findMember.setName("HelloJPA");
+
+            //JPQL - 객체가 대상인 쿼리문..
+            List<Member> result = em.createQuery("select m from Member as m", Member.class) //대부분의 쿼리 자성 가능.
+                    .setFirstResult(5)  //페이징 쉽게 가능.
+                    .setMaxResults(10)
+                    .getResultList();
 
 
-       em.close();
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
        emf.close();
     }
 }
